1f7082e87e9fb72720c7837e1f6ee9b5790ad6a2
Rawkriguez <rawkriguez@yahoo.com>
2024-12-09 03:19:37 -0600

    finished declarative update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-//import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-//const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
 
-//root.render(
-//  <h1>Hello, React</h1>
-//)
+root.render(
+  <h1 className='header'>Hello, React</h1>
+)
 
 
 /*
